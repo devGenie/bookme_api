7cc51741bcda100f112d2b92909201e032cc5313
Onen Julius <jonen54@gmail.com>
2017-11-06 16:39:30 +0300

    Add test subject

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { described_class.new }
   it "has a valid factory" do
     FactoryBot.create(:user).should be_valid
   end
@@ -11,12 +12,12 @@ RSpec.describe User, type: :model do
   end
 
   it "is not valid without a name" do 
-    user = User.new(first_name:nil)
-    expect(user).to_not be_valid
+    subject.first_name=nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid with missing attributes" do
-    user = User.new(first_name:"Onen")
-    expect(user).to_not be_valid
+    subject.first_name ="Onen"
+    expect(subject).to_not be_valid
   end
 end
