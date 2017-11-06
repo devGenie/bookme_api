1285fcf4ea5ec0e0d12560bdd8dd9285f99385ce
Onen Julius <jonen54@gmail.com>
2017-11-06 22:40:05 +0300

    test for duplicate emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { described_class.new }
+  subject { described_class.new(first_name:"Onen",
+                                last_name:"Julius",
+                                email:"jonen54@gmail.com",
+                                password:"256thjuly",
+                                date_added:Time.now(),
+                                created_at:Time.now(),
+                                updated_at:Time.now())
+                               }
   it "has a valid factory" do
     FactoryBot.create(:user).should be_valid
   end
@@ -11,19 +18,19 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it "is not valid without a name" do 
-    subject.first_name=nil
+  it "is not valid without an email" do 
+    subject.email=nil
     expect(subject).to_not be_valid
   end
 
   it "is not valid with missing attributes" do
-    subject.first_name ="Onen"
+    subject.first_name =nil
     expect(subject).to_not be_valid
   end
   
   it "is not valid if duplicate" do
     original_user = FactoryBot.create(:user)
-    duplicate = FactoryBot.create(:user)
+    duplicate = subject
     expect(duplicate).to_not be_valid
   end
 end
