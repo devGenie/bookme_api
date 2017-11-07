6d2dfc7295f4b0be60d44c5cd9ecade2d51c652d
Onen Julius <jonen54@gmail.com>
2017-11-07 16:05:18 +0300

    Add user subcription relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :subscriptions, dependent: :destroy
     validates_presence_of :first_name, :last_name, :email, :password
     validates_uniqueness_of :email
 end
