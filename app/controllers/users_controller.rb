3afaf93aee9515eeb944d2d6e59809d009058dab
Onen Julius <jonen54@gmail.com>
2017-11-08 21:04:35 +0300

    Add create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,28 @@ class UsersController < ApplicationController
         json_response(@user)
     end
 
+    def create
+        if user_params['password'] == user_params['repeat_password']
+            new_params=user_params.except(:repeat_password)
+            current_time = Time.now()
+            new_params[:date_added] = current_time
+            new_params[:created_at] = current_time
+            new_params[:updated_at] = current_time
+            @user = User.create!(new_params)
+            json_response(@user, :created)
+        end
+    end
+
     private
-    
+
+    def user_params
+        params.permit(:first_name, 
+                      :last_name,
+                      :email,
+                      :password,
+                      :repeat_password)
+    end
+
     def get_user
         @user = User.find(params[:id])
     end
