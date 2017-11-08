106423cd29e2199d53528822eb4c498ab2a2f6fa
Onen Julius <jonen54@gmail.com>
2017-11-08 19:23:46 +0300

    Add test for single user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Users API", type: :request do
     let!(:users) { FactoryBot.create_list(:random_user,10) }
+    let(:user_id) { users.first.id}
     describe 'GET /users' do
         before { get '/users' }
         
@@ -12,6 +13,17 @@ RSpec.describe "Users API", type: :request do
 
         it 'returns status code 200' do
             expect(response).to have_http_status(200)
+        end
+    end
+
+    describe 'GET /users/:id' do
+        before { get "/users/#{user_id}"}
+
+        context 'when the user exists' do
+            it 'returns the user' do
+                expect(json).not_to be_empty
+                expect(json['id']).to eq(user_id)
+            end
         end
     end
 end
