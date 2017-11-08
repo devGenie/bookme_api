d944611e197e6f6ceead7ac76e4b9806fd2a9f16
Onen Julius <jonen54@gmail.com>
2017-11-08 18:50:57 +0300

    Add random users to factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,19 @@
+require 'faker'
 FactoryBot.define do
-    factory :user do |u|
-        u.first_name "Onen"
-        u.last_name "Julius"
-        u.email "jonen54@gmail.com"
-        u.password "256thjuly"
-        u.date_added Time.now()
-        u.created_at Time.now()
-        u.updated_at Time.now()
+    factory :user do
+        first_name "Onen"
+        last_name "Julius"
+        email "jonen54@gmail.com"
+        password "256thjuly"
+        date_added Time.now()
+        created_at Time.now()
+        updated_at Time.now()
+
+        factory :random_user do
+            first_name { Faker::Name.first_name }
+            last_name  { Faker::Name.last_name}
+            email      { Faker::Internet.email}
+            password   "password"
+        end
     end
 end
