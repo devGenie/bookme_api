3a07bfe5f584718152ac7ce8d6a33f1378287abf
Onen Julius <jonen54@gmail.com>
2017-11-08 19:36:37 +0300

    Add status code test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -24,6 +24,10 @@ RSpec.describe "Users API", type: :request do
                 expect(json).not_to be_empty
                 expect(json['id']).to eq(user_id)
             end
+
+            it 'returns status code 200' do
+                expect(response).to have_http_status(200)
+            end
         end
     end
 end
