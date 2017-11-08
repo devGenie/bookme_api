00a5a34affe068fc1a0fc14c70c9e0b2648ee8bb
Onen Julius <jonen54@gmail.com>
2017-11-08 19:26:32 +0300

    Add single user endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+    before_action :get_user, only: [:show, :update, :destroy]
     def index
         @users=User.all
         json_response(@users)
+    end
+
+    def show
+        json_response(@user)
+    end
+
+    def get_user
+        @user = User.find(params[:id])
     end
 end
