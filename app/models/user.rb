21169e596259d27621068da9948f30a4f7be25d1
Onen Julius <jonen54@gmail.com>
2017-11-09 12:31:00 +0300

    Add hashed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :subscriptions, dependent: :destroy
-    validates_presence_of :first_name, :last_name, :email, :password
+    validates_presence_of :first_name, :last_name, :email, :password_digest
     validates_uniqueness_of :email
 end
