06f498d6b306a553d1b9d141c6cd4fc43ab9612f
Onen Julius <jonen54@gmail.com>
2017-11-11 16:22:39 +0300

    Remove sensitive fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
     before_action :get_user, only: [:show, :update, :destroy]
     def index
-        @users=User.all
+        @users=User.select('id',
+                           'first_name',
+                           'last_name',
+                           'email',
+                           'date_added',
+                           'updated_at')
         json_response(@users)
     end
 
@@ -14,9 +19,9 @@ class UsersController < ApplicationController
             new_params=user_params.except(:repeat_password)
             current_time = Time.now()
             new_params[:date_added] = current_time
-            new_params[:created_at] = current_time
             new_params[:updated_at] = current_time
             @user = User.create!(new_params)
+            @user = @user.attributes.except("password_digest")
             json_response(@user, :created)
         end
     end
