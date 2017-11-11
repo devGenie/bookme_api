f838f0ee420f7396f02126319066b6aece7f7de2
Onen Julius <jonen54@gmail.com>
2017-11-11 17:55:52 +0300

    Add user authentication table

---
+++ db/migrate/20171111142127_create_authentications.rb
@@ -0,0 +1,13 @@
+class CreateAuthentications < ActiveRecord::Migration[5.1]
+  def change
+    create_table :authentications do |t|
+      t.string    :token, null:false, unique:true
+      t.references :user, foreign_key: true, null:false
+      t.datetime :created_on, null:false, default: -> { 'CURRENT_TIMESTAMP' } 
+      t.datetime :expires_at, null:false
+      t.boolean  :status, null:false, default:true
+      t.timestamps
+    end
+    add_index :authentications, :token
+  end
+end
