104cd9e7f9944c6a1d99eda632500006da48d1d7
Onen Julius <jonen54@gmail.com>
2017-11-13 19:55:26 +0300

    Add authentication barebones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 require 'json_web_token'
+require 'macaddr'
 class ApplicationController < ActionController::API
     include Response
+
+    protected
+    def authenticate_request!
+    end
+
+    def invalidate_authentication
+    end
+
+    private 
+    def payload
+    end
+
+    def load_current_user!
+    end
+
 end
