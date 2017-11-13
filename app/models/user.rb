7f5072bff577da6b96c6995b1ad5a710bb50af59
Onen Julius <jonen54@gmail.com>
2017-11-13 11:54:50 +0300

    Downcase email before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+    before_save :downcase_email
     has_secure_password
     has_many :subscriptions, dependent: :destroy
     has_many :authentications, dependent: :destroy
     validates_presence_of :first_name, :last_name, :email, :password_digest
     validates_uniqueness_of :email
     validates_format_of :email, with: /@/ 
+
+    def downcase_email
+        self.email = self.email.delete(' ').downcase
+    end
 end
