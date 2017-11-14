d55ff21f28cac3e547732b2fa397e7241c9f9d04
Onen Julius <jonen54@gmail.com>
2017-11-14 17:44:44 +0300

    Add relationship with blacklisted_tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     before_save :downcase_email
     has_secure_password
     has_many :subscriptions, dependent: :destroy
-    has_many :authentications, dependent: :destroy
+    has_many :blacklisted_tokens, dependent: :destroy
     validates_presence_of :first_name, :last_name, :email, :password_digest
     validates_uniqueness_of :email
     validates_format_of :email, with: /@/ 
