fbdcb6c0f90492549b7c615b56cbed1ab8ad33bb
Onen Julius <jonen54@gmail.com>
2017-11-14 13:01:44 +0300

    Add token authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,29 @@ class ApplicationController < ActionController::API
 
     protected
     def authenticate_request!
+        if !payload
+            invalid_authentication
+        else
+            load_current_user!
+            invalid_authentication unless @current_user
+        end
     end
 
-    def invalidate_authentication
+    def invalid_authentication
+        json_response({status:'failed',message:'Your request is invalid or expired'},:unauthorized)
     end
 
     private 
     def payload
+        auth_header = request.headers['Authorization']    
+        token = auth_header
+        JsonWebToken.verify(token, key: ENV['hash_key'])
+
+    rescue
+        nil                                                                                                                                                                                                                                                                                                                                            
     end
 
     def load_current_user!
+        @current_user = User.find_by_id(3)
     end
-
 end
