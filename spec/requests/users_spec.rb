8f4fd7e62be809e011cf3178382f575348b0a43d
Onen Julius <jonen54@gmail.com>
2017-11-15 17:11:37 +0300

    Add test for login endpoint

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,9 +3,17 @@ require "rails_helper"
 RSpec.describe "Users API", type: :request do
     let!(:users) { FactoryBot.create_list(:random_user,10) }
     let(:user_id) { users.first.id}
+    let(:valid_fields) { { first_name:"Onen", 
+                           last_name:"Julius", 
+                           email:"jonen54@gmail.com",
+                           password:"256thjuly",
+                           repeat_password:"256thjuly"} }
+    let(:login_details){{
+                           email:valid_fields[:email],
+                           password:valid_fields[:params]}}
+    
     describe 'GET /users' do
         before { get '/users' }
-        
         it 'returns users' do
             expect(json).not_to be_empty
             expect(json.size).to eq(10)
@@ -32,12 +40,6 @@ RSpec.describe "Users API", type: :request do
     end
 
     describe 'POST /users' do
-        let(:valid_fields) { { first_name:"Onen", 
-                               last_name:"Julius", 
-                               email:"jonen54@gmail.com",
-                               password:"256thjuly",
-                               repeat_password:"256thjuly"} }
-
         context('when the request is valid') do
             before{ post '/users', params:valid_fields }
 
@@ -47,6 +49,27 @@ RSpec.describe "Users API", type: :request do
 
             it 'returns status 201' do
                 expect(response).to have_http_status(201)
+            end
+        end
+    end
+
+    describe 'POST /users/login' do
+        before :each do
+            @user = FactoryBot.create(:user)
+        end
+
+        context('user is able to login') do
+            before{ post '/users/login', params:{email:@user.email,password:@user.password}}
+            it 'user can login after registration' do
+                expect(response).to have_http_status(200)
+            end
+
+            it 'after login, token should be returned' do
+                expect(json).to have_key('token')
+            end
+
+            it 'After login, correct message should be returned' do
+                expect(json['message']).to eq("User logged in successfully")
             end
         end
     end
