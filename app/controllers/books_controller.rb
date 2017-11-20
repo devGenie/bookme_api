234c4bdee225453dc86e4560a684d45030c4ba86
Onen Julius <jonen54@gmail.com>
2017-11-20 19:05:54 +0300

    Create accepted book parameters

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,33 @@
 class BooksController < ApplicationController
+    def index
+        json_response({lib_id:params[:library_id]})
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def get_library
+        @library_id = params[:library_id]
+    end
+
+    def book_params
+        params.permit(:title, 
+                      :isbn, 
+                      :copies,
+                      :desription,
+                      :author_id,  
+                      :cover_type,
+                      :category
+                      )
+    end
 end
