d17facfaa54136f3c47b9961d470fb49ad534450
Onen Julius <jonen54@gmail.com>
2017-11-20 20:06:39 +0300

    Add categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,20 @@
 class CategoriesController < ApplicationController
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def category_params
+    end
 end
