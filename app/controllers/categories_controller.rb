344cf268c5088bf71e53cb239dfdffaeddeb8c6b
Onen Julius <jonen54@gmail.com>
2017-11-22 11:59:03 +0300

    Create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,24 @@
 class CategoriesController < ApplicationController
+    before_action :authenticate_request!, only:[:index,
+                                               :show,
+                                               :create,
+                                               :update,
+                                               :destroy]
+    
     def index
     end
 
     def create
+        category = Category.new
+        category.name = category_params[:name]
+        category.user = @current_user
+        if category.save!
+            json_response({status:'success',
+                           message:'Category created successfully',
+                           category:category},:created)
+        else
+            json_response({status:'failed',message:'Category failed to create'})
+        end
     end
 
     def show
@@ -16,5 +32,6 @@ class CategoriesController < ApplicationController
 
     private
     def category_params
+        params.permit(:name)
     end
 end
